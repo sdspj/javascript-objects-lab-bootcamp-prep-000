edf098105ab16ad4a146d087855a0870a8fc99a5
Philly Kelly <p_a_k_@hotmail.com>
2017-12-21 22:18:37 +0000

    automatically backed up by learn

---
+++ cheese.js
@@ -0,0 +1,8 @@
+var recipes = new Object({ eggs: 'cheese' });
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, { key: value })
+}
+
+
+updateObjectWithKeyAndValue(recipes, cheese, eggs)
